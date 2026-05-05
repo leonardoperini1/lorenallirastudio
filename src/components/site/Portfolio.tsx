79c105aa468f306e889344cd59e7067f8cae1804
leonardoperini1 <leoaperini@gmail.com>
2026-05-05 14:53:20 -0300

    Change portfolio image from JPG to GIF format

--- src/components/site/Portfolio.tsx
+++ src/components/site/Portfolio.tsx
@@ -5,7 +5,7 @@ import heroRose from "@/assets/hero-rose.jpg";
 import { AudioCard } from "./AudioCard";
 import p1 from "@/assets/portfolio-1.jpg";
 import p2 from "@/assets/portfolio-2.jpg";
-import p3 from "@/assets/portfolio-3.jpg";
+import p3 from "@/assets/portfolio-3.gif";
 import p4 from "@/assets/portfolio-4.jpg";
 import p5 from "@/assets/portfolio-5.jpg";
 import p6 from "@/assets/portfolio-6.jpg";
